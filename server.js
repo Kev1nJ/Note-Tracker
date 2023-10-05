aa5e167241b743e832aa8a176e8c922a90393de0
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-10-04 22:18:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-app.use(express.static(".Develop/public"));
+// Import the feedback router
+const routes = require('./Develop/routes');
 
-const app = express();
-const port = process.env.PORT || 3001;
+const PORT = 3001;
+
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static('./Develop/public'));
+
+app.use('/', routes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
